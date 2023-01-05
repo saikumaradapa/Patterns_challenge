71278a4755ebf790e1897ee65c5ad644a750728e
ADAPA SAI KUMAR <96902883+saikumaradapa@users.noreply.github.com>
2023-01-05 22:21:29 +0530

    Update Patterns_challenge.py

--- Patterns_challenge.py
+++ Patterns_challenge.py
@@ -213,18 +213,52 @@ for i in range(1,n) :
 """
 
 # A 
-# for i in range(7) :
-#     for j in range(5) :
-#         # if ((j == 0 or j == 4) ) or (i == 0 and j != 0) or ( i == 3 ) :
-#         if ((j ==0 or j == 4 ) and i != 0) or ((i ==0 or i ==3) and (j > 0 and j < 4)) :
-#             print("* ", end=" ")
-#         else :
-#             print("  ", end=" ")
-#     print()
+for i in range(7) :
+    for j in range(5) :
+        # if ((j == 0 or j == 4) ) or (i == 0 and j != 0) or ( i == 3 ) :
+        if ((j ==0 or j == 4 ) and i != 0) or ((i ==0 or i ==3) and (j > 0 and j < 4)) :
+            print("* ", end=" ")
+        else :
+            print("  ", end=" ")
+    print()
 
 
 
 
+"""pattern C
+
+  * * * * 
+*     
+*     
+*     
+*     
+*     
+ * * * * 
+
+ """
+# C
+for row in range(7) :
+    for col in range(5) :
+        if (col==0 and (row!=0 and row!=6)) or ((row==0 or row==6) and (col>0)) :
+            print("*", end =" ")
+        else :
+            print("",end=" ")
+    print()
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 
 
 # n = 10
